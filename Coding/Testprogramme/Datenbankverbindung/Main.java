f0c790d1ef2f2dffd3e206cc118912c56af5d816
KriegersBlog <53866271+KriegersBlog@users.noreply.github.com>
2019-10-14 23:42:36 +0200

    test

---
+++ Coding/Testprogramme/Datenbankverbindung/Main.java
@@ -0,0 +1,11 @@
+// Autor: Julian Krieger
+// Datum: 14.10.2019
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MySQLAccess dao = new MySQLAccess();
+        dao.readDataBase();
+    }
+
+}
+
