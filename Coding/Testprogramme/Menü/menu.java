356ae1657905643c51eca3a730ed3472057b9fff
KriegersBlog <53866271+KriegersBlog@users.noreply.github.com>
2019-10-16 20:07:07 +0200

    s

--- Coding/Testprogramme/Menü/menu.java
+++ Coding/Testprogramme/Menü/menu.java
@@ -8,10 +8,20 @@ import javax.swing.event.*;
 
 public class menu extends JFrame {
   // Anfang Attribute
-  private JMenuBar jMenuBar1 = new JMenuBar();
-  private JMenu jMenu1 = new JMenu("Java");
-  private JMenuItem jMenuItem1 = new JMenuItem("Sucks");
-  private JMenuItem jMenuItem2 = new JMenuItem("Hard");
+  private JMenuBar menu_bar = new JMenuBar();
+  
+  private JMenu menu_erstellen = new JMenu("Erstellen");
+  private JMenu menu_aendern = new JMenu("Ändern");
+  private JMenu menu_abfragen = new JMenu("Abfragen");
+  
+  private JMenuItem item_abfragen_artikel = new JMenuItem("Artikel");
+  private JMenuItem item_abfragen_personal = new JMenuItem("Peronal");
+  
+  private JMenuItem menu_aendern_artikel = new JMenuItem("Artikel");
+  private JMenuItem menu_aendern_personal = new JMenuItem("Personal");
+  
+  private JMenuItem menu_erstellen_artikel = new JMenuItem("Artikel");
+  private JMenuItem menu_erstellen_personal = new JMenuItem("Personal");
   // Ende Attribute
   
   public menu() { 
@@ -30,29 +40,49 @@ public class menu extends JFrame {
     Container cp = getContentPane();
     cp.setLayout(null);
     // Anfang Komponenten
-    jMenu1.add(jMenuItem1);
-    setJMenuBar(jMenuBar1);
-    jMenuBar1.add(jMenu1);
+    menu_aendern.add(menu_aendern_personal);
+    menu_aendern.add(menu_aendern_artikel);
+    menu_erstellen.add(menu_erstellen_artikel);
+    menu_erstellen.add(menu_erstellen_personal);
+    menu_abfragen.add(item_abfragen_artikel);
+    menu_abfragen.add(item_abfragen_personal);
+    
+    menu_bar.add(menu_erstellen);
+    menu_bar.add(menu_aendern);
+    menu_bar.add(menu_abfragen);
     // jMenu1.setMnemonic(KeyEvent.VK_A);  Tastenkürzel um Menü zu öffnen
     // Ende Komponenten
-    setJMenuBar(jMenuBar1);
-    jMenu1.add(jMenuItem2); 
+     
+    setJMenuBar(menu_bar);
     setVisible(true);
     
-    jMenuItem1.addActionListener(new ActionListener() {
+    menu_aendern_artikel.addActionListener(new ActionListener() {
             // Beim Drücken des Menüpunktes wird actionPerformed aufgerufen
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Sucks");
             }
     }); 
     
-    jMenuItem2.addActionListener(new ActionListener() {
+    menu_aendern_personal.addActionListener(new ActionListener() {
             // Beim Drücken des Menüpunktes wird actionPerformed aufgerufen
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Hard");
             }
     });
     
+    menu_erstellen_artikel.addActionListener(new ActionListener() {
+            // Beim Drücken des Menüpunktes wird actionPerformed aufgerufen
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Sucks");
+            }
+    }); 
+    
+    menu_erstellen_personal.addActionListener(new ActionListener() {
+            // Beim Drücken des Menüpunktes wird actionPerformed aufgerufen
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Hard");
+            }
+    });
   }
   
   // Anfang Methoden
